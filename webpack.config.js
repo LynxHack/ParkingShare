36d78f34879a570a94e3a2b08a63a3fde13316bb
CopterDoctor <Jordananders@me.com>
2018-07-27 16:27:25 -0700

    .gitignore build files

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "source-map",
   entry: "./src/client/index.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "public/dist"),
     filename: "bundle.js",
     publicPath: "/build/"
   },
