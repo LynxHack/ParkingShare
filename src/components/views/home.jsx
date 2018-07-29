5af3b2f70d60b772754561f1dab9e85f5aab8b3b
LynxHack <KevinLynx_L@hotmail.com>
2018-07-29 00:47:53 +0000

    changed class to className in react component for homepage

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -8,21 +8,19 @@ export default class Home extends Component {
   render() {
     return (
       <div id='main'>
-      <body>
-      <div id="header">
-        <div id="nav">
-            <p id="logo_text">SpotSharer</p>
-            <a href="#" class="nav_links">Log In</a>
-            <a href="#" class="nav_links">Sign Up</a>
-            <a href="#" class="nav_links"></a>
-            <a href="#" id="host_link">Share A Spot</a>
-        </div> 
-      </div>
-      <div id="welcome_text_div">
-        <p id="welcome_text">Welcome To Your Spot</p>
-        <p id="welcome_under_text">Rent out uninque parking spots all around the greatest city in the world.</p>
-      </div>     
-      </body>
+        <div id="header">
+          <div id="nav">
+              <p id="logo_text">SpotSharer</p>
+              <a href="#" className="nav_links">Log In</a>
+              <a href="#" className="nav_links">Sign Up</a>
+              <a href="#" className="nav_links"></a>
+              <a href="#" id="host_link">Share A Spot</a>
+          </div> 
+        </div>
+        <div id="welcome_text_div">
+          <p id="welcome_text">Welcome To Your Spot</p>
+          <p id="welcome_under_text">Rent out uninque parking spots all around the greatest city in the world.</p>
+        </div>     
       </div>
     );
   }
