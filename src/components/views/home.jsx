99cc100200cb0cd81fc521dfcbc9356dcf45c43e
klynx <KevinLynx_L@hotmail.com>
2018-07-30 10:04:50 -0700

    Fixed nav bar css

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -9,6 +9,11 @@ export default class Home extends Component {
       <div id="welcome_text_div">
         <h1>Welcome To Your Spot</h1>
         <p>Rent out uninque parking spots all around the greatest city in the world.</p>
+
+        <form class="search-container"/>
+        <input type="text" id="search-bar" placeholder="What can I help you with today?"/>
+        <a href="#"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
+      
       </div>
     );
   }
