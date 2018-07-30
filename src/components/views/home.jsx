befe69de7b531daad87db7d6ace25ce86cba256c
klynx <KevinLynx_L@hotmail.com>
2018-07-30 16:28:30 -0700

    Added transparency and modified search bar in home page

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -14,7 +14,9 @@ export default class Home extends Component {
           <input type="text" id="search-bar" placeholder="What can I help you with today?"/>
           <a href="#"><img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></a>
         </form>
-        
+
+        <footer className = 'homefooter'>
+        </footer>
       </div>
     );
   }
