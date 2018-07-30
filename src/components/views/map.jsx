7f2956221da41d4e71e7953111b7db44956d4103
CopterDoctor <Jordananders@me.com>
2018-07-29 21:03:16 -0700

    implement getting token from server through env variable

--- src/components/views/map.jsx
+++ src/components/views/map.jsx
@@ -1,53 +1,47 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { resolve } from 'url';
 require('./../../stylesheets/map.scss');
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      token: "didn't work",
-      width: "500px",
-      height: "500px"
     }
-    // this.requestToken = this.requestToken.bind(this);
+    this.requestToken = this.requestToken.bind(this);
   }
 
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   if (nextProps.height !== prevState.height || nextProps.width !== prevState.width) {
-  //     return ({ height: nextProps.height, width: nextProps.width }) // <- this is setState equivalent
-  //   }
-  //   return null
-  // }
-
-  async componentDidMount() {
-    
-    mapboxgl.accessToken = ""
-    const map = new mapboxgl.Map({
-      container: "map",
-      center: [-123.1092519, 49.2696751],
-      zoom: 13,
-      style: "mapbox://styles/jordananders/cjk7bfdek7ceu2rlkbyq440qp"
+  componentDidMount() {
+    this.requestToken().then((token) => {
+      mapboxgl.accessToken = token;
+      const map = new mapboxgl.Map({
+        container: "map",
+        center: [-123.1092519, 49.2696751],
+        zoom: 13,
+        style: "mapbox://styles/jordananders/cjk7bfdek7ceu2rlkbyq440qp"
+      });
     });
   }
 
-  // requestToken() {
-  //   fetch('http://localhost:8080/token')
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (myJson) {
-  //       this.setState({ token: { myJson } });
-  //     })
-  //     .catch((error) => {
-  //       console.log(`Error is response : ${error}`);
-        
-  //     })
-  // }
+  requestToken() {
+    return new Promise((resolve, reject) => {
+      fetch("http://localhost:8080/token")
+        .then((response) => {
+          return response.json();
+        })
+        .then((myJson) => {
+          resolve(myJson);
+        })
+        .catch((error) => {
+          console.log(`Error in fetching token : ${error}`);
+        })
+    })
+  }
 
   render() {
     return (
-      <div id="map"></div>
+      <div id="map">
+      </div>
     )
   }
 }
