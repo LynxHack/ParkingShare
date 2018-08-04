998834376d467eebd6ced894b26b80e819581329
Jashan <jashan.khela@hotmail.com>
2018-08-04 01:08:23 +0000

    update to fix CSS on user page

--- src/components/views/user.jsx
+++ src/components/views/user.jsx
@@ -9,6 +9,22 @@ export default class userPage extends Component {
   render() {
     return (
 <div className="userSection">
+<section className="sidebar">
+
+
+  <img src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg" class="image--cover"/>
+
+  <h3> Welcome User </h3>
+  <h4>Email : </h4>
+  <h4>User@example.com</h4>
+  <h4>Phone Number</h4><i class="fa fa-phone" aria-hidden="true"></i>
+
+
+  <h4>1-(800)-000-1234</h4>
+
+
+
+ </section>
 <section className="reservations">
 <h3> Your Reservations </h3>
 <div id="container">            
@@ -60,9 +76,61 @@ export default class userPage extends Component {
 
 </div> 
 
+</section>
+
+<section className="spots">
+<h3> Your Parking Spots </h3>
+<div id="container">            
+
+       <article class="new-tweet-article">
+      <header>
+        <h3>Spot ID: </h3>
+
+     </header>
+      <div class="tweet-body">
+      <p class ="message">     
+       <p class ="">Address : </p>
+      <p class ="">City : </p>
+      <p class ="">Postal Code : </p>
+      </p>
+      </div>
+    </article>
+
+       <article class="new-tweet-article">
+      <header>
+        <h3>Spot ID: </h3>
+
+     </header>
+      <div class="tweet-body">
+      <p class ="message">     
+       <p class ="">Address : </p>
+      <p class ="">City : </p>
+      <p class ="">Postal Code : </p>
+      </p>
+      </div>
+    </article>
+
+       <article class="new-tweet-article">
+      <header>
+        <h3>Spot ID: </h3>
+
+     </header>
+      <div class="tweet-body">
+      <p class ="message">     
+       <p class ="">Address : </p>
+      <p class ="">City : </p>
+      <p class ="">Postal Code : </p>
+      </p>
+      </div>
+    </article>
+
+</div> 
+
 
 
 </section>
+
+
 
 
 
