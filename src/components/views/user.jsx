f35399d256e915dee94786cad8a983bc29849d1f
CopterDoctor <Jordananders@me.com>
2018-08-03 18:34:44 -0700

    fixed <p> as child error

--- src/components/views/user.jsx
+++ src/components/views/user.jsx
@@ -4,142 +4,112 @@ import axios from 'axios';
 
 
 export default class userPage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       reservations: [] //to populate with userregistration objects
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.post('/getreservations', {})
-    .then((res) => {
-      for(let i = 0; i < res.data.length; i++){
-        this.setState((prevState) => {
-          reservations: prevState.reservations.push({
-            resnum: i,
-            address: res.data[i].address,
-            city: res.data[i].city,
-            postalcode: res.data[i].postalcode,
-            stall: res.data[i].stall,
-            buzzer: res.data[i].buzzer,
-            starttime: res.data[i].startimeunix,
-            endtime: res.data[i].endtimeunix
+      .then((res) => {
+        for (let i = 0; i < res.data.length; i++) {
+          this.setState((prevState) => {
+            reservations: prevState.reservations.push({
+              resnum: i,
+              address: res.data[i].address,
+              city: res.data[i].city,
+              postalcode: res.data[i].postalcode,
+              stall: res.data[i].stall,
+              buzzer: res.data[i].buzzer,
+              starttime: res.data[i].startimeunix,
+              endtime: res.data[i].endtimeunix
+            })
           })
-        })
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
-  generatereservationcard(res){
+  generatereservationcard(res) {
     console.log(res);
-    return(
+    return (
       <article className="new-tweet-article">
-      <header>
-        <h3>Reservation {res.resnum}</h3>
-        <h5> {res.starttime} - {res.endtime} </h5>
-     </header>
-      <div className="tweet-body">
-      <p className ="message">     
-       <p className ="">Address : {res.address} </p>
-      <p className ="">City : {res.city}</p>
-      <p className ="">Postal Code : {res.postalcode}</p>
-      <p className ="">Stall #  : {res.stall}</p>
-      <p className ="">Buzzer #  : {res.buzzer}</p></p>
-      </div>
-    </article>
+        <header>
+          <h3>Reservation {res.resnum}</h3>
+          <h5> {res.starttime} - {res.endtime} </h5>
+        </header>
+        <div className="tweet-body">
+          <p className="message"></p>
+          <p className="">Address : {res.address} </p>
+          <p className="">City : {res.city}</p>
+          <p className="">Postal Code : {res.postalcode}</p>
+          <p className="">Stall #  : {res.stall}</p>
+          <p className="">Buzzer #  : {res.buzzer}</p>
+        </div>
+      </article>
     )
   }
 
   render() {
     return (
-<div className="userSection">
-<section className="sidebar">
-
-
-  <img src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg" class="image--cover"/>
-
-  <h3> Welcome User </h3>
-  <h4>Email : </h4>
-  <h4>User@example.com</h4>
-  <h4>Phone Number</h4><i class="fa fa-phone" aria-hidden="true"></i>
-
-
-  <h4>1-(800)-000-1234</h4>
-
-
-
- </section>
-<section className="reservations">
-<h3> Your Reservations </h3>
-<div id="container">            
-{this.generatereservationcard(this.state.reservations)}
-
-</div> 
-
-</section>
-
-<section className="spots">
-<h3> Your Parking Spots </h3>
-<div id="container">            
-
-       <article class="new-tweet-article">
-      <header>
-        <h3>Spot ID: </h3>
-
-     </header>
-      <div class="tweet-body">
-      <p class ="message">     
-       <p class ="">Address : </p>
-      <p class ="">City : </p>
-      <p class ="">Postal Code : </p>
-      </p>
+      <div className="userSection">
+        <section className="sidebar">
+          <img src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg" className="image--cover" />
+          <h3> Welcome User </h3>
+          <h4>Email : </h4>
+          <h4>User@example.com</h4>
+          <h4>Phone Number</h4><i className="fa fa-phone" aria-hidden="true"></i>
+          <h4>1-(800)-000-1234</h4>
+        </section>
+        <section className="reservations">
+          <h3> Your Reservations </h3>
+          <div id="container">
+            {this.generatereservationcard(this.state.reservations)}
+          </div>
+        </section>
+        <section className="spots">
+          <h3> Your Parking Spots </h3>
+          <div id="container">
+            <article className="new-tweet-article">
+              <header>
+                <h3>Spot ID: </h3>
+              </header>
+              <div className="tweet-body">
+                <p className="message"></p>
+                <p className="">Address : </p>
+                <p className="">City : </p>
+                <p className="">Postal Code : </p>
+              </div>
+            </article>
+            <article className="new-tweet-article">
+              <header>
+                <h3>Spot ID: </h3>
+              </header>
+              <div className="tweet-body">
+                <p className="message"></p>
+                <p className="">Address : </p>
+                <p className="">City : </p>
+                <p className="">Postal Code : </p>
+              </div>
+            </article>
+            <article className="new-tweet-article">
+              <header>
+                <h3>Spot ID: </h3>
+              </header>
+              <div className="tweet-body">
+                <p className="message"></p>
+                <p className="">Address : </p>
+                <p className="">City : </p>
+                <p className="">Postal Code : </p>
+              </div>
+            </article>
+          </div>
+        </section>
       </div>
-    </article>
-
-       <article class="new-tweet-article">
-      <header>
-        <h3>Spot ID: </h3>
-
-     </header>
-      <div class="tweet-body">
-      <p class ="message">     
-       <p class ="">Address : </p>
-      <p class ="">City : </p>
-      <p class ="">Postal Code : </p>
-      </p>
-      </div>
-    </article>
-
-       <article class="new-tweet-article">
-      <header>
-        <h3>Spot ID: </h3>
-
-     </header>
-      <div class="tweet-body">
-      <p class ="message">     
-       <p class ="">Address : </p>
-      <p class ="">City : </p>
-      <p class ="">Postal Code : </p>
-      </p>
-      </div>
-    </article>
-
-</div> 
-
-
-
-</section>
-
-
-
-
-
-</div>
-
     );
   }
 }
