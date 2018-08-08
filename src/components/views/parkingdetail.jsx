de7d5f0ba6b8142713d4701f5c480d875a9320e2
LynxHack <KevinLynx_L@hotmail.com>
2018-08-08 15:06:22 +0000

    Check user login to enable review section

--- src/components/views/parkingdetail.jsx
+++ src/components/views/parkingdetail.jsx
@@ -120,6 +120,32 @@ export default class ParkingDetail extends Component {
     })
   }
 
+  checklogin(){
+    console.log(this.props);
+    if(this.props.isLoggedIn){
+      return(
+                            
+        <div className="reviewsection"> 
+        <textarea name="review" onChange={this.updatereview.bind(this)}  placeholder="Write a review"/>
+        <div className="productbutton submit blueSubmit left" onClick={this.submitreview.bind(this)}>Submit Review</div> 
+       
+        <fieldset class="rating" onClick={this.updaterating.bind(this)}>
+            <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+            <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+            <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+            <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+            <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+            <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+            <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+            <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+            <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+            <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+        </fieldset>
+        </div>        
+      )
+    }
+  }
+
   render() {
     return (       
       <div className="parkingdetail">
@@ -180,12 +206,14 @@ export default class ParkingDetail extends Component {
               {this.state.reviews && this.state.reviews.map(this.generatereviews)}
 {/*               
               this.generatereviews(this.state.reviews) */}
-                    
-                    <div className="reviewsection"> 
+                    {this.checklogin.bind(this)()}
+                    {/* <div className="reviewsection"> 
                       <textarea name="review" onChange={this.updatereview.bind(this)}  placeholder="Write a review"/>
                       <div className="productbutton submit blueSubmit left" onClick={this.submitreview.bind(this)}>Submit Review</div> 
                      
-                      <fieldset class="rating" onClick={this.updaterating.bind(this)}>
+
+
+                       <fieldset class="rating" onClick={this.updaterating.bind(this)}>
                           <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
                           <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
                           <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
@@ -196,8 +224,8 @@ export default class ParkingDetail extends Component {
                           <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
                           <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
                           <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-                      </fieldset>
-                      </div>        
+                      </fieldset> 
+                                            </div>         */}
                    </div>             
                   
                 </div>  
