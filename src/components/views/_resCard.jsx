7a877a5258d8f925acd9d2cc49b648d2d59e1aed
CopterDoctor <Jordananders@me.com>
2018-08-08 09:48:15 -0700

    res card key changed again

--- src/components/views/_resCard.jsx
+++ src/components/views/_resCard.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 module.exports = {
   resCard: function (props) {
     return (
-      <article className="new-tweet-article" key={`res${props.id}`}>
+      <article className="new-tweet-article" key={`res${props.id}${props.starttimeunix}`}>
         <header>
           <h3>Reservation {props.resnum}</h3>
           <h5> {moment.unix(props.starttimeunix).format("MM/DD/YYYY")} / {moment.unix(props.endtimeunix).format("MM/DD/YYYY")} </h5>
